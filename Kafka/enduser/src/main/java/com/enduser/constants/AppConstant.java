898259daf0a8fe710245728555e52c631a227cfd
sunilsahare <61555396+sunilsahare@users.noreply.github.com>
2023-09-16 18:14:05 +0530

    Delete Kafka directory

--- Kafka/enduser/src/main/java/com/enduser/constants/AppConstant.java
+++
@@ -1,6 +0,0 @@
-package com.enduser.constants;
-
-public class AppConstant {
-    public static final String LOCATION_UPDATE_TOPIC = "location-update-topic";
-    public static final String GROUP_ID = "l1";
-}
